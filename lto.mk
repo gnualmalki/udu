cb5bf8e64762dd411ca6003373f83d887594eb62
ali. <gnualmalki@gmail.com>
2025-11-30 11:59:53 +0000

    use one line syntax to fix bsd make on macOS ci

--- lto.mk
+++ lto.mk
@@ -1,8 +1,7 @@
 #
 # Check if compiler sup LTO
 #
-LTO_OK := $(shell echo 'int main(){return 0;}' | \
-           $(CC) -x c - -flto -o /dev/null 2>/dev/null && echo "OK")
+LTO_OK := $(shell echo 'int main(){return 0;}' | $(CC) -x c - -flto -o /dev/null 2>/dev/null && echo "OK")
 
 ifeq ($(LTO_OK),OK)
     CFLAGS  += -flto
--- omp.mk
+++ omp.mk
@@ -1,9 +1,7 @@
 #
 # check if openmp version met the requirements
 #
-OMP_VER := $(shell echo '#include <omp.h>' | \
-			$(CC) -x c - -dM -E -fopenmp 2>/dev/null | \
-			awk '/_OPENMP/ {print $$3}')
+OMP_VER := $(shell echo '#include <omp.h>' | $(CC) -x c - -dM -E -fopenmp 2>/dev/null | awk '/_OPENMP/ {print $$3}')
 
 ifeq ($(OMP_VER),)
     OMP_MSG := disabled (OpenMP < 3.0 detected or not available)
